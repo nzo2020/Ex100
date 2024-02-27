3e2a270ad3901fb75c29ddaa802d9b0d19dddae4
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-02-27 15:07:25 +0200

    Ex100

--- app/src/main/java/com/example/ex10/MainActivity.java
+++ app/src/main/java/com/example/ex10/MainActivity.java
@@ -23,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
